800e1231e7ef847438b70b96ec7760ba0b5e93af
Yury Shakhmatov <shufflepkt@yandex.ru>
2023-01-19 19:32:06 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("10", "101"));
+        System.out.println(bins.mult("101", "111"));
+    }
+}
